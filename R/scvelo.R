9b94456c5a7791ebc4b25adaba3a9c3d203ec849
Charlotte Soneson <charlottesoneson@gmail.com>
2020-06-14 21:13:39 +0200

    Add x matrix to use for dimension reduction

--- R/scvelo.R
+++ R/scvelo.R
@@ -2,17 +2,21 @@
 #'
 #' Perform RNA velocity calculations with the \pkg{scVelo} package.
 #'
-#' @param x A list of two matrices of the same dimensions where genes are in rows and cells are in columns.
-#' The first matrix should contain spliced counts and the second matrix should contain unspliced counts.
-#' If \code{norm=TRUE}, both matrices should contain normalized expression values instead.
+#' @param x A list of three matrices of the same dimensions where genes are in rows and cells are in columns.
+#' The first matrix should contain the counts used for calculating reduced dimension representations and cell-to-cell distances.
+#' The second matrix should contain spliced counts and the third matrix should contain unspliced counts.
+#' If \code{norm=TRUE}, all matrices should contain normalized (but not log-transformed) expression values instead.
 #'
-#' Alternatively, a \linkS4class{SummarizedExperiment} object containing two such matrices in its assays.
+#' Alternatively, a \linkS4class{SummarizedExperiment} object containing three such matrices among its assays.
 #' @param ... For the generic, further arguments to pass to specific methods.
-#' For the SummarizedExperiment method, further arguments to pass to the ANY method.
+#' For the SummarizedExperiment and SingleCellExperiment methods, further arguments to pass to the ANY method.
+#' @param assay.x An integer scalar or string specifying the assay of \code{x} containing the counts that will be used for dimension reduction and distance calculations.
 #' @param assay.spliced An integer scalar or string specifying the assay of \code{x} containing the spliced counts.
 #' @param assay.unspliced An integer scalar or string specifying the assay of \code{x} containing the unspliced counts.
 #' @param norm Logical scalar indicating whether the matrices in \code{x} are already normalized.
-#' In such cases, \code{sf.spliced} and \code{sf.unspliced} are ignored.
+#' In such cases, \code{sf.x}, \code{sf.spliced} and \code{sf.unspliced} are ignored.
+#' @param sf.x A numeric vector containing size factors for the counts in \code{x} for each cell.
+#' Defaults to \code{\link{librarySizeFactors}} on the \code{x} count matrix.
 #' @param sf.spliced A numeric vector containing size factors for the spliced counts for each cell.
 #' Defaults to \code{\link{librarySizeFactors}} on the spliced matrix.
 #' @param sf.unspliced A numeric vector containing size factors for the unspliced counts for each cell.
@@ -21,11 +25,14 @@
 #' Defaults to all genes but is most typically set to a subset of interesting genes, e.g., highly variable genes.
 #' Note that, if set, any subsetting is done \emph{after} normalization so that library sizes are correctly computed.
 #' @param use.theirs Logical scalar indicating whether \pkg{scVelo}'s gene filtering and normalization should be used,
-#' in which case \code{sf.spliced}, \code{sf.unspliced} and \code{subset.row} are ignored.
+#' in which case \code{sf.counts}, \code{sf.spliced}, \code{sf.unspliced} and \code{subset.row} are ignored.
 #' @param mode String specifying the method to use to estimate the transcriptional dynamics.
 #' @param scvelo.params List of lists, providing arguments for scVelo functions.
 #' @param dimred A low-dimensional representation of the cells (N x k matrix, where N is the number of cells) that will be used to find the nearest neighbors for the moment calculations.
 #' Alternatively, if \code{x} is a \linkS4class{SingleCellExperiment}, a character scalar with the name of a reduced dimension representation in \code{x}.
+#' @param ncomponents Numeric scalar indicating the number of principal components to obtain, if \code{use.theirs} is FALSE and no \code{dimred} is supplied.
+#' @param BSPARAM A \linkS4class{BiocSingularParam} object specifying which algorithm should be used to perform the PCA.
+#' @param BPPARAM A \linkS4class{BiocParallelParam} object specifying whether the PCA calculations should be parallelized.
 #'
 #' @details
 #' This function uses the \pkg{scVelo} package (\url{https://pypi.org/project/scvelo/}) to perform RNA velocity calculations.
@@ -68,23 +75,28 @@
 #' spliced <- counts(sce1)
 #' unspliced <- counts(sce2)
 #'
-#' out <- scvelo(list(spliced, unspliced), use.theirs=TRUE)
+#' out <- scvelo(list(spliced, spliced, unspliced), use.theirs=TRUE)
 #'
-#' @author Aaron Lun
+#' @author Aaron Lun, Charlotte Soneson
 #' @name scvelo
 NULL
 
 #' @importFrom S4Vectors DataFrame
 #' @importFrom scuttle normalizeCounts
+#' @importFrom BiocSingular bsparam
+#' @importFrom BiocParallel SerialParam
 .scvelo <- function(x, subset.row=NULL, norm=FALSE,
-    sf.spliced=NULL, sf.unspliced=NULL,
+    sf.x=NULL, sf.spliced=NULL, sf.unspliced=NULL,
     use.theirs=FALSE,
     mode=c('steady_state', 'deterministic', 'stochastic', 'dynamical'),
-    scvelo.params=list(), dimred=NULL)
+    scvelo.params=list(), dimred=NULL, ncomponents=30, BPPARAM=SerialParam(),
+    BSPARAM=bsparam())
 {
-    spliced <- x[[1]]
-    unspliced <- x[[2]]
-    if (!identical(as.integer(dim(spliced)), as.integer(dim(unspliced)))) {
+    spliced <- x[[2]]
+    unspliced <- x[[3]]
+    x <- x[[1]]
+    if (!all(identical(as.integer(dim(spliced)), as.integer(dim(unspliced))),
+             identical(as.integer(dim(spliced)), as.integer(dim(x))))) {
         stop("matrices in 'x' must have the same dimensions")
     }
 
@@ -93,30 +105,31 @@ NULL
         stop("need raw counts to use the 'scvelo' native pipeline")
     }
 
-    # If use.theirs = FALSE, normalization but no log-transformation
-    # will be applied to spliced and unspliced abundances. However,
-    # if scVelo calculates the PCA it will assume that X (here set to
-    # spliced) is log-transformed
-    if (!use.theirs && is.null(dimred)) {
-        stop("if use.theirs=FALSE, dimred must be supplied")
-    }
-
     if (!use.theirs) {
         if (!norm) {
+            x <- normalizeCounts(x, sf.x, log=TRUE)
             spliced <- normalizeCounts(spliced, sf.spliced, log=FALSE)
             unspliced <- normalizeCounts(unspliced, sf.unspliced, log=FALSE)
+        } else {
+            x <- log2(x + 1)
         }
 
         if (!is.null(subset.row)) {
+            x <- x[subset.row,,drop=FALSE]
             spliced <- spliced[subset.row,,drop=FALSE]
             unspliced <- unspliced[subset.row,,drop=FALSE]
+        }
+
+        if (is.null(dimred)) {
+            dimred <- BiocSingular::runPCA(t(x), rank=ncomponents,
+                                           BSPARAM=BSPARAM, BPPARAM=BPPARAM)
         }
 
     }
 
     mode <- match.arg(mode)
     output <- basiliskRun(env=velo.env, fun=.run_scvelo,
-        spliced=spliced, unspliced=unspliced,
+        x=x, spliced=spliced, unspliced=unspliced,
         use.theirs=use.theirs, mode=mode,
         scvelo.params=scvelo.params,
         dimred=dimred)
@@ -126,19 +139,23 @@ NULL
 
 #' @importFrom reticulate import
 #' @importFrom DelayedArray is_sparse t
-.run_scvelo <- function(spliced, unspliced, use.theirs=FALSE, mode='dynamical', scvelo.params=list(), dimred=NULL) {
+.run_scvelo <- function(x, spliced, unspliced, use.theirs=FALSE, mode='dynamical', scvelo.params=list(), dimred=NULL) {
+    x <- t(.make_np_friendly(x))
     spliced <- t(.make_np_friendly(spliced))
     unspliced <- t(.make_np_friendly(unspliced))
+
     if (!is.null(dimred)) {
         dimred <- .make_np_friendly(dimred)
     }
 
     and <- import("anndata")
     scv <- import("scvelo")
-    adata <- and$AnnData(spliced, layers=list(spliced=spliced, unspliced=unspliced))
+    adata <- and$AnnData(x, layers=list(spliced=spliced, unspliced=unspliced))
     adata$obs_names <- rownames(spliced)
     adata$var_names <- colnames(spliced)
-    if (!use.theirs && !is.null(dimred)) {
+
+    ## A supplied dimred will be used even if use.theirs=TRUE
+    if (!is.null(dimred)) {
         adata$obsm <- list(X_pca = dimred)
     }
 
@@ -211,25 +228,27 @@ setMethod("scvelo", "ANY", .scvelo)
 
 #' @export
 #' @rdname scvelo
+#' @importFrom BiocGenerics sizeFactors
 setMethod("scvelo", "SummarizedExperiment", function(x, ...,
-    assay.spliced="counts", assay.unspliced="unspliced",
-    sf.spliced=NULL, sf.unspliced=NULL)
+    assay.x="counts", assay.spliced="spliced", assay.unspliced="unspliced",
+    sf.x=sizeFactors(x), sf.spliced=NULL, sf.unspliced=NULL)
 {
-    scvelo(list(assay(x, assay.spliced), assay(x, assay.unspliced)), ..., sf.spliced=sf.spliced, sf.unspliced=sf.unspliced, dimred=NULL)
+    scvelo(list(assay(x, assay.x), assay(x, assay.spliced), assay(x, assay.unspliced)), ..., sf.x=sf.x, sf.spliced=sf.spliced, sf.unspliced=sf.unspliced, dimred=NULL)
 })
 
 #' @export
 #' @rdname scvelo
+#' @importFrom BiocGenerics sizeFactors
 #' @importFrom SingleCellExperiment reducedDimNames reducedDim
 setMethod("scvelo", "SingleCellExperiment", function(x, ...,
-    assay.spliced="counts", assay.unspliced="unspliced",
-    sf.spliced=NULL, sf.unspliced=NULL, dimred="PCA")
+    assay.x="counts", assay.spliced="spliced", assay.unspliced="unspliced",
+    sf.x=sizeFactors(x), sf.spliced=NULL, sf.unspliced=NULL, dimred="PCA")
 {
     if (!is.null(dimred) && dimred %in% reducedDimNames(x)) {
         dimred <- reducedDim(x, dimred)
     } else {
-        dimred=NULL
+        dimred <- NULL
     }
 
-    scvelo(list(assay(x, assay.spliced), assay(x, assay.unspliced)), ..., sf.spliced=sf.spliced, sf.unspliced=sf.unspliced, dimred=dimred)
+    scvelo(list(assay(x, assay.x), assay(x, assay.spliced), assay(x, assay.unspliced)), ..., sf.x=sf.x, sf.spliced=sf.spliced, sf.unspliced=sf.unspliced, dimred=dimred)
 })
