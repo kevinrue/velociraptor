ffdc10c6ca22563ef238b01f364e5a0a40a9c66f
Charlotte Soneson <charlottesoneson@gmail.com>
2020-07-20 08:48:00 +0200

    Specify that .AnnDataDependencies comes from zellkonverter

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,4 +1,4 @@
 #' @importFrom basilisk BasiliskEnvironment
 #' @importFrom zellkonverter .AnnDataDependencies
 velo.env <- BasiliskEnvironment("env", "velociraptor",
-    packages=.AnnDataDependencies, pip="scvelo==0.2.1")
+    packages=zellkonverter::.AnnDataDependencies, pip="scvelo==0.2.1")
