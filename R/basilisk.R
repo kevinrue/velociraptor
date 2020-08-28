36acd86929fc3fa3f10f56b100a8650767157e09
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2020-08-28 21:26:13 +0100

    bump version of scvelo package

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,4 +1,4 @@
 #' @importFrom basilisk BasiliskEnvironment
 #' @importFrom zellkonverter .AnnDataDependencies
 velo.env <- BasiliskEnvironment("env", "velociraptor",
-    packages=zellkonverter::.AnnDataDependencies, pip="scvelo==0.2.1")
+    packages=zellkonverter::.AnnDataDependencies, pip="scvelo==0.2.2")
