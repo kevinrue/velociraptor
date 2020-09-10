6d3ffb1b9d34c6c52a6f6601103d4aba7c3977c8
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2020-09-10 20:52:11 +0100

    Fix BiocCheck and downsample dataset in vignette  (#16)

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# velociraptor 0.99.0
+
+* First submission to Bioconductor.
--- R/basilisk.R
+++ R/basilisk.R
@@ -1,4 +1,17 @@
+# scvelo 0.2.2 and latest version of dependencies in zellkonverter::.AnnDataDependencies succesfully tested.
+.scvelo_dependencies <- c(
+    "scvelo==0.2.2",
+    "anndata==0.7.4",
+    "h5py==2.10.0",
+    "hdf5==1.10.6",
+    "natsort==7.0.1",
+    "numpy==1.19.1",
+    "packaging==20.4",
+    "pandas==1.1.2",
+    "scipy==1.5.2"
+)
+
 #' @importFrom basilisk BasiliskEnvironment
 #' @importFrom zellkonverter .AnnDataDependencies
 velo.env <- BasiliskEnvironment("env", "velociraptor",
-    packages=zellkonverter::.AnnDataDependencies, pip="scvelo==0.2.1")
+    packages=.scvelo_dependencies, channels = c("bioconda", "conda-forge"))
