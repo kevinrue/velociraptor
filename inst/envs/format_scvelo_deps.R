4d246ebd9b0233e029e7bc675124740bb8c64ef7
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2024-05-23 13:51:57 +0100

    add Linux environment

--- inst/envs/format_scvelo_deps.R
+++ inst/envs/format_scvelo_deps.R
@@ -1,18 +1,19 @@
 library(dplyr)
 library(tidyr)
 
-pkg_info <- read.table("M1_20240522.txt")
+pkg_info <- read.table("Linux_20240523.txt")
 pkg_specs <- pkg_info %>%
     as_tibble() %>%
-    unite("spec", V1, V2, sep = "==")
-%>% pull(spec)
+    unite("spec", V1, V2, sep = "==") %>%
+    pull(spec)
 
 cat(paste0(
     "c(",
     "\n",
     paste0("  ", "'", pkg_specs, "'", collapse = ",\n"),
     "\n",
-    ")"
+    ")",
+    "\n"
 ))
 
 # paste the result in R/basilisk.R
