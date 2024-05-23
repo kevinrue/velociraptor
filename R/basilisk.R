846eab712851fcfd0f637056591e20016e0f3664
Kevin Rue-Albrecht <kevinrue@users.noreply.github.com>
2024-05-23 12:54:13 +0100

    add dependencies required for the same reason as M1

--- R/basilisk.R
+++ R/basilisk.R
@@ -2,7 +2,12 @@
 # and dependencies pinned to fix various issues linked below
 .scvelo_dependencies <- list(
   "x86_64" = c(
-    "scvelo==0.2.5"
+    "scvelo==0.2.5",
+    "matplotlib==3.7.3", # https://stackoverflow.com/questions/77128061/ydata-profiling-profilereport-attributeerror-module-matplotlib-cbook-has-no
+    "tqdm==4.66.4", # required for progress bar`
+    "ipywidgets==8.1.2", # required for progress bar
+    "jupyterlab==4.2.0", # required for progress bar
+    "numpy==1.23.1" # https://github.com/OpenTalker/video-retalking/issues/35
   ),
   "aarch64" = c(
     'absl-py==2.1.0',
