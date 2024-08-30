c46195e9e9f0d9a4a46d189e9f48678d3cf73ada
Kevin Rue <kevinrue@users.noreply.github.com>
2024-08-30 08:09:31 +0100

    add pip dependencies

--- inst/envs/format_yaml_to_r.R
+++ inst/envs/format_yaml_to_r.R
@@ -1,11 +1,32 @@
 library(yaml)
-yaml_data <- yaml::read_yaml("Linux_x86_64_anaconda.yaml")
-r_data <- gsub("([[:alnum:]_]+)=([[:alnum:].]+)=.+", "\\1==\\2" , yaml_data$dependencies)
+yaml_data <- yaml::read_yaml("Windows.yaml")
+
+which_pip_dependencies <- which(sapply(yaml_data$dependencies, function(x) any(names(x) %in% "pip")))
+
+conda_dependencies <- yaml_data$dependencies[-c(which_pip_dependencies)]
+
+conda_dependencies <- gsub("([[:alnum:]_]+)=([[:alnum:].]+)=.+", "\\1==\\2" , conda_dependencies)
+
+
+message("Conda dependencies:")
 cat(paste0(
   "c(",
   "\n",
-  paste0("  ", "'", r_data, "'", collapse = ",\n"),
+  paste0("  ", "'", conda_dependencies, "'", collapse = ",\n"),
   "\n",
   ")",
   "\n"
 ))
+
+message("pip dependencies:")
+if (length(which_pip_dependencies)) {
+  pip_dependencies <- yaml_data$dependencies[[which_pip_dependencies]]$pip
+  cat(paste0(
+    "c(",
+    "\n",
+    paste0("  ", "'", pip_dependencies, "'", collapse = ",\n"),
+    "\n",
+    ")",
+    "\n"
+  ))
+}
